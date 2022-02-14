ae75c4eed3bea1b08cb6f85ae749957a87cef18e
OAK <osamakhan.Ok83@gmail.com>
2022-02-14 21:54:35 +0500

    Day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catalogue/home_page.dart';
+import 'package:flutter_catalogue/pages/home_page.dart';
+import 'package:flutter_catalogue/pages/login_page.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,9 +9,18 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return MaterialApp(
-      home: HomePage(),        
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => HomePage(),
+        "/home": (context) => HomePage(),
+        "/Login": (context) => LoginPage(),
+      },
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -4,19 +4,18 @@ class HomePage extends StatelessWidget {
   final int days = 30;
   final String name = "OAK";
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Catalogue App"),
+      appBar: AppBar(
+        title: Text("Catalogue App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter with $name"),
         ),
-        body: Center(
-            child: Container(
-      child: Text("Welcome to $days days of flutter with $name"),
-    ),
-    ),
-    drawer: Drawer(),
+      ),
+      drawer: Drawer(),
     );
   }
 }
